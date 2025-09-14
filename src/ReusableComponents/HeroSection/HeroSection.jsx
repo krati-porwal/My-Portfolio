d51967cdd71c4fd2ce1864f57d238266da526e4f
kratiDatacode <krati.datacode@gmail.com>
2025-09-14 16:59:37 +0530

    modify images of projects

--- src/ReusableComponents/HeroSection/HeroSection.jsx
+++ src/ReusableComponents/HeroSection/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = ({
 }) => {
   return (
     <section className="hero-section py-5 text-white">
-      <div className="row justify-content-center text-center">
+      <div className="row py-5 justify-content-center text-center">
         <div className="col-md-10">
           <h1 className="display-4 fw-bold mb-3">
             {title} <span className="text-gradient">{highlight}</span>
